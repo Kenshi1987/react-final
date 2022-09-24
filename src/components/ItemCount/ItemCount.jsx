e87be79db34f730e85d4ea027c3f95d59aa981ea
Kenshi1987 <diegocabe87@gmail.com>
2022-09-24 20:15:09 -0300

    Se agrega ItemDetail y ItemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import {Button} from 'react-bootstrap'
 
-export default function ItemCount() {
-  return (
-    <div>ItemCount</div>
-  )
+export default function ItemCount({product}) {
+ 
+ const [count, setCount] = useState(1);
+ function sum(){
+  setCount(count + 1);
+
+ }
+
+ function res (){
+  setCount(count - 1);
+
+ }
+
+ function onAdd(){
+  alert('Quiero Agregar' + count + "de este Articulo" + JSON.stringify(product));
+ }
+  
+ 
+     return (
+    <div className='m-5'>
+      
+      <Button className="btn btn-success btn-lg m-3" onClick={res} disabled={count===1}>-</Button>
+      <span className='fs-4 fw-bold'>{count}</span>
+      <Button className="btn btn-success btn-lg m-3" onClick={sum} disabled={count===product.stock}>+</Button>
+      <Button className="btn btn-success btn-lg m-3" onClick={()=>onAdd(count)} disabled={count ===0}>Agregar al Carrito</Button>
+    
+      
+    </div>
+  );
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ItemCount from '../ItemCount/ItemCount';
 
 export default function ItemDetail({product}) {
   return (
     
       <div className="d-flex align-items-center justify-content-center text-center">
-    <Card style={{ width: '80%', height:'auto', margin:'100px' }}>
+    <Card className='border border-3'style={{ width: '80%', height:'auto', margin:'100px' }}>
       <Card.Title className='text-center fs-1 m-3'>{product.name}</Card.Title>
       <Card.Img style={{height:'auto', width:'100%', padding:'10px'}} src={product.img} />
       <Card.Body>
         
         <Card.Text className='justify fs-3'>
           {product.description}
-        </Card.Text>       
+        </Card.Text> 
+        <Card.Text className='justify fs-6'>
+          Stock={product.stock}          
+        </Card.Text> 
+        <Card.Text className='justify fs-2 fw-bold'>
+       $ {product.price}          
+        </Card.Text>            
       </Card.Body>
+      <ItemCount product={product}/>
     </Card>
     </div>
     
